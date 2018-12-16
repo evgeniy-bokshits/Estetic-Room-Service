f78d400cf4dd2b2e4ff868ca0218ec6f94bdccc2
evgeniy-bokshits <evgeniy.bokshits@gmail.com>
2018-12-17 00:00:43 +0300

    Added Estetic Room Service test

---
+++ EsteticRoomBackend/Controllers/MessagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EsteticRoomBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        static List<Models.Message> messages = new List<Models.Message> {
+            new Models.Message
+            {
+                Owner = "John",
+                Text = "Hello!"
+            },
+            new Models.Message
+            {
+                Owner = "Tim",
+                Text = "Hi!"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Models.Message> Get()
+        {
+            return messages;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Models.Message message)
+        {
+            messages.Add(message);
+        }
+    }
+}
